dc235b009bea682e661a99cfd21b6cacf8194e52
romansychok <romansychok@gmail.com>
2017-11-16 23:59:40 +0200

    I'm Senior

--- src/main/java/ua/com/owu/entity/User.java
+++ src/main/java/ua/com/owu/entity/User.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Entity
 public class User implements UserDetails {
-
+///////////////I'm Senior
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
